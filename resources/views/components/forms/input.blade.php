1026e4b8691c32491f29722ab2759c74fcc4c81c
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-27 22:44:11 +0400

    Potato sacs done

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label for="data-{{$name}}">{{ ucfirst($name)}} {{$label}}</label>
+    <label for="data-{{$name}}">{{$label ?? ucfirst($name)}}</label>
     <input type="{{$type}}" class="form-control" id="data-{{$name}}" name="{{$name}}" value="{{ old($name) ?? $value }}">
     @error($name)
         <p class="text-danger">{{ $message }}</p>
--- resources/views/livewire/show-sacs.blade.php
+++ resources/views/livewire/show-sacs.blade.php
@@ -10,9 +10,9 @@
                 <hr class="m-1">
                 <div class="row d-flex align-items-center m-0">
                     <input type="hidden"  name="sacs[{{$index}}][id]"    value="{{$sac['id']}}" />
-                    <x-forms.input        name="sacs[{{$index}}][name]" :value="$sac['name']" />
-                    <x-forms.input        name="sacs[{{$index}}][sac_count]"  :value="$sac['sac_count']" />
-                    <x-forms.input        name="sacs[{{$index}}][sac_weight]"  :value="$sac['sac_weight']" />
+                    <x-forms.input        name="sacs[{{$index}}][name]" :value="$sac['name']" label="Sac name"/>
+                    <x-forms.input        name="sacs[{{$index}}][sac_count]"  :value="$sac['sac_count']" label="Sac count"/>
+                    <x-forms.input        name="sacs[{{$index}}][sac_weight]"  :value="$sac['sac_weight']" label="Sac weight"/>
                     @if($action)
                         <div class="form-group col-12 col-md-1 mb-3 mb-md-0 mt-0 mt-md-3 pl-3 pl-md-0">
                             <button type="button" wire:click.prevent="removeSac({{$index}})" class="btn btn-outline-danger">
