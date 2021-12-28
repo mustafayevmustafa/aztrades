b73e6e4ac6d3b17600b03f1870afa95dfac69d24
Mustafa Mustafayev <mustafayevmustafa99@gmail.com>
2021-12-28 22:55:37 +0400

    City County Seeder

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CityRequest;
+use App\Http\Requests\CountryRequest;
+use App\Models\City;
+use App\Models\Country;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+
+class CityController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('Admin.cities.index')->with([
+            'cities' => City::get()
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.cities.edit', [
+            'action' => route('cities.store'),
+            'method' => null,
+            'data'   => null
+        ]);
+    }
+
+    public function store(CityRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $city = City::create($validated);
+
+        return redirect()->route('cities.index')->with('success', "City {$city->getAttribute('name')} reated successfully!");
+    }
+
+    public function show(City $city)
+    {
+        return view('admin.cities.edit', [
+            'action' => null,
+            'method' => null,
+            'data'   => $city
+        ]);
+    }
+
+    public function edit(City $city)
+    {
+        return view('admin.countries.edit', [
+            'action' => route('cities.update', $city),
+            'method' => "PUT",
+            'data'   => $city
+        ]);
+    }
+
+    public function update(CityRequest $request, City $city): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $city->update($validated);
+
+        return redirect()->route('cities.index')->with('success', "City {$city->getAttribute('name')} updated successfully!");
+    }
+
+    public function destroy(City $city): JsonResponse
+    {
+        if($city->delete()){
+            return response()->json(['code' => 200]);
+        }else{
+            return response()->json(['code' => 400]);
+        }
+    }
+}
---
+++ resources/views/Admin/cities/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('Admin.layout.master')
+
+@section('content')
+    <div class="row" xmlns:x-forms="http://www.w3.org/1999/html">
+        <div class="col-12">
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="page-title mb-0 font-size-18">Şəhərlər</h4>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <a class="btn btn-outline-primary" href="{{route('cities.index')}}"><i class="mdi mdi-arrow-left"></i></a>
+                        @if (is_null($action))
+                            <a class="btn btn-outline-primary" href="{{route('cities.edit', $data)}}">Edit</a>
+                        @endif
+                    </div>
+                    <form action="{{ $action }}" method="POST" enctype="multipart/form-data">
+                        @csrf @method($method)
+                        @if ($action)
+                        @endif
+                        <div class="form-group">
+                            <label for="post-title">Şəhər Adı</label>
+                            <input type="text" value="{{ optional($data)->getAttribute('name') }}" name="name" class="form-control" id="post-title" placeholder="Ölkə Adı Daxil Edin">
+                            @error('name')
+                            <p class="text-danger">
+                                {{ $message }}
+                            </p>
+                            @enderror
+                        </div>
+                        @if ($action)
+                            <button type="submit" class="btn btn-primary">Əlavə Et</button>
+                        @endif
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@section('script')
+    @if (is_null($action))
+        <script>
+            $('form :input').attr('disabled', true)
+        </script>
+    @endif
+@endsection
+@endsection
