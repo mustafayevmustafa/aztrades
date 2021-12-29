07a993813846544aa52c65bb42b1856ee080bbbf
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-29 21:21:56 +0400

    website template added

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aztrade Company MMC</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="header">
+  <div class="header-left-panel">
+    <div class="logo-wrap">
+      <div class="logo">
+        <center><h1><img src="{{asset('assets/images/logo.jpeg')}}" width="250" height="250"/></h1></center>
+        <center><h2>Hər kəs üçün faydalı</h2></center>
+      </div>
+    </div>
+  </div>
+  <div class="header-right-panel">
+    <div class="header-right-panel-top">
+      <p>Əlaqə telefonu : +994 51 745 45 30
+      <p> <a href="#">Mail adresimiz: info@aztradecompany.az</a>
+    </div>
+    <div class="menu">
+      <ul>
+        <li class="marRight20"><a class="active" href="index.blade.php">Əsas səhifə</a></li>
+        <li class="marRight20"><a href="#about.html">Biz kimik</a></li>
+        <li><a href="#services.html">Xidmətlərimiz</a></li>
+        <li class="marRight20"><a href="#features.html">Təkliflərimiz</a></li>
+        <li class="marRight20"><a href="#">Partniyorlarımız</a></li>
+        <li><a href="#contact.html">Əlaqə</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!--- header div end -->
+<div class="panel-wrap">
+  <div class="panel-wrapper">
+    <div class="panel marRight30">
+      <div class="img"><img src="images/img-1.jpg" width="260" height="260" /></div>
+      <div class="title">
+        <h1>Yerli Soğan</h1>
+      </div>
+      <div class="border"></div>
+      <div class="content">
+        <p>Yüksək səviyyədə seçilib yığılmış kisələrdə təqdim edirik.</p>
+        <div class="button-link"><a href="#">0.39 AZN</a></div>
+      </div>
+    </div>
+    <div class="panel marRight30">
+      <div class="img"><img src="images/img-2.jpg" width="260" height="260"/></div>
+      <div class="title">
+        <h1 class="border-bottom">KARTOF İRAN</h1>
+      </div>
+      <div class="border"></div>
+      <div class="content">
+        <p>İran İslam Respublikasından standartlara uyğun idxal edilən məhsul təqdim edirik<br />
+        </p>
+        <div class="button-link"><a href="#">0.78 AZN</a></div>
+      </div>
+    </div>
+    <div class="panel">
+      <div class="img"><img src="images/img-3.jpg" width="260" height="260"/></div>
+      <div class="title">
+        <h1 class="border-bottom">KARTOF UKRAYNA</h1>
+      </div>
+      <div class="border"></div>
+      <div class="content">
+        <p>Ukraynadan sizlər üçün seçilmiş əla növ kartofun idxalı həyata keçirilir.</p>
+        <div class="button-link"><a href="#">0.80 AZN</a></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--- panel wrap div end -->
+
+
+<!--- page wrap div end -->
+<div class="footer">
+  <p>Copyright (c)  Bütün hüquqları qorunur.</p>
+</div>
+</body>
+</html>
