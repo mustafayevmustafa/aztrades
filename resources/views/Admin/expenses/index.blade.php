59832c45833295400e1154d85061ccea044e40ea
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-01-12 09:54:43 +0400

    rates fixed

--- resources/views/Admin/expenses/index.blade.php
+++ resources/views/Admin/expenses/index.blade.php
@@ -73,7 +73,7 @@
                             </thead>
                             <tbody>
                             @foreach ($expenses as $expense)
-                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #8ed4af" @endif>
+                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #b4f6d3" @endif>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $expense->getAttribute('customer') }}</td>
                                     <td>{{ $expense->getRelationValue('type')->getAttribute('name') }}</td>
