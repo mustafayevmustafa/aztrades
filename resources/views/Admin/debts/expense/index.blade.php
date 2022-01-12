833062aea123d9dd2164e5fb6505711801073cc3
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-01-12 09:52:51 +0400

    rates fixed

--- resources/views/Admin/debts/expense/index.blade.php
+++ resources/views/Admin/debts/expense/index.blade.php
@@ -62,7 +62,7 @@
                             </thead>
                             <tbody>
                             @foreach ($expenses as $expense)
-                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #4aa0e6" @endif>
+                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #6dd69f" @endif>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $expense->getAttribute('customer') }}</td>
                                     <td>{{ $expense->goodsType()->exists() ? ($expense->getRelationValue('goodsType')->getTable() == 'onions' ? 'Soğan' : 'Kartof') : 'Digər' }}</td>
--- resources/views/Admin/debts/income/index.blade.php
+++ resources/views/Admin/debts/income/index.blade.php
@@ -60,7 +60,7 @@
                             </thead>
                             <tbody>
                             @foreach ($expenses as $expense)
-                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #4aa0e6" @endif>
+                                <tr @if($expense->getAttribute('closed_rate_id')) style="background-color: #6dd69f" @endif>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $expense->getAttribute('customer') }}</td>
                                     <td>{{ $expense->getAttribute('note') }}</td>
