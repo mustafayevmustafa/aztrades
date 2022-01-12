93dacfbdb824cae4545d99ce84f0fd87f3379e61
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-01-12 09:54:01 +0400

    rates fixed

--- resources/views/Admin/sellings/index.blade.php
+++ resources/views/Admin/sellings/index.blade.php
@@ -63,7 +63,7 @@
                             </thead>
                             <tbody>
                             @forelse($sellings as $selling)
-                                <tr @if($selling->getAttribute('closed_rate_id')) style="background-color: #6dd69f" @endif>
+                                <tr @if($selling->getAttribute('closed_rate_id')) style="background-color: #8ed4af" @endif>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $selling->getAttribute('customer') }}</td>
                                     <td>{{ $selling->getAttribute('type') == 'onion' ? "Soğan" : "Kartof" }}</td>
