43932b1c0d3f83d70b0c988a673cf41590c624cf
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-01-16 23:58:34 +0400

    bugs fixed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -74,8 +74,6 @@ class DashboardController extends Controller
 
     public function test()
     {
-        foreach (Expense::where('expense_type_id', 8)->get() as $e) {
-            $e->selling()->update(['sellingable_type' => $e->getAttribute('goods_type')]);
-        }
+        return 'testing area';
     }
 }
